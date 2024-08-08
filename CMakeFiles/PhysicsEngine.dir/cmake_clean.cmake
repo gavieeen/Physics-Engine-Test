65491eb1a8f0756d00881666503e7fd148945009
gavieeen <gavinebenezer@gmail.com>
2024-08-07 23:09:04 -0500

    init commit

---
+++ CMakeFiles/PhysicsEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysicsEngine.dir/src/Object.cpp.o"
+  "CMakeFiles/PhysicsEngine.dir/src/Object.cpp.o.d"
+  "CMakeFiles/PhysicsEngine.dir/src/PhysicsEngine.cpp.o"
+  "CMakeFiles/PhysicsEngine.dir/src/PhysicsEngine.cpp.o.d"
+  "CMakeFiles/PhysicsEngine.dir/src/main.cpp.o"
+  "CMakeFiles/PhysicsEngine.dir/src/main.cpp.o.d"
+  "PhysicsEngine"
+  "PhysicsEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysicsEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
